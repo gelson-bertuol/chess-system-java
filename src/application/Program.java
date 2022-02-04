4302076d0024a67270cb64ad54b3b9ac19afab42
Gelson Bertuol <gelson.bertuol@gmail.com>
2022-02-03 22:21:42 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,6 +39,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotino (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				System.out.println("Press <Enter> to continue");
